1a743eff4e54cdff568639cf558abd227df230a0
Valentin Rault <rault.valentin@gmail.com>
2020-11-15 11:12:54 +0100

    addition of .gitignore

--- covid_project/covid_project/settings.py
+++ covid_project/covid_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .secret import secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$#dehygq3k5+@f(w6kix^o7(=robj_@ow*xddx6t0lz7p%k#s$'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
