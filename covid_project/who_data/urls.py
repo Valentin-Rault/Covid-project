700b5dc9829e807f9963e8f61c1a91a1131b7a97
Valentin Rault <rault.valentin@gmail.com>
2020-11-24 10:47:14 +0100

    Changed who_data to back-end with rest_framwork and creation of serializers and api view addition of rest_framework to installed app

--- covid_project/who_data/urls.py
+++ covid_project/who_data/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
-from .views import WhoDataListView
+from .views import WhoDataView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='who_data/base.html'), name='home'),
-    path('db_list', WhoDataListView.as_view(), name='db_list'),
+    path('who-data/', WhoDataView.as_view(), name='db_list'),
 ]
